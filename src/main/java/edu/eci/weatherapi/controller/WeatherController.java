5f7543d3b9c77870b26708353cac628e39825ae8
sancarbar <sancarbar@gmail.com>
2021-08-26 18:52:04 -0500

    Connected Controller with MongoDB Repository.

--- src/main/java/edu/eci/weatherapi/controller/WeatherController.java
+++ src/main/java/edu/eci/weatherapi/controller/WeatherController.java
@@ -2,6 +2,8 @@ package edu.eci.weatherapi.controller;
 
 import edu.eci.weatherapi.data.document.Weather;
 import edu.eci.weatherapi.data.dto.WeatherDto;
+import edu.eci.weatherapi.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,27 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController
 {
 
+    private final WeatherService weatherService;
+
+    public WeatherController( @Autowired WeatherService weatherService )
+    {
+        this.weatherService = weatherService;
+    }
+
     @PostMapping
     public Weather createWeather( @RequestBody WeatherDto weatherDto )
     {
-        return new Weather( weatherDto );
+
+        return weatherService.create( new Weather( weatherDto ) );
+
+
     }
 
     @GetMapping( "/{id}" )
     public Weather findWeatherById( @PathVariable String id )
     {
-        return null;
+        return weatherService.findById( id );
     }
 
     @DeleteMapping( "/{id}" )
     public boolean deleteWeatherById( @PathVariable String id )
     {
-        return false;
+        return weatherService.deleteById( id );
     }
 
     @PutMapping( "/{id}" )
     public Weather updateWeatherById( @RequestBody WeatherDto weatherDto, @PathVariable String id )
     {
-        return null;
+        return weatherService.updateById( weatherDto, id );
     }
 }
--- src/main/java/edu/eci/weatherapi/data/document/Weather.java
+++ src/main/java/edu/eci/weatherapi/data/document/Weather.java
@@ -1,12 +1,15 @@
 package edu.eci.weatherapi.data.document;
 
 import edu.eci.weatherapi.data.dto.WeatherDto;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
-import java.util.UUID;
 
+@Document
 public class Weather
 {
+    @Id
     private String id;
 
     private String description;
@@ -17,12 +20,48 @@ public class Weather
 
     private String location;
 
+    public Weather()
+    {
+    }
+
     public Weather( WeatherDto weatherDto )
     {
-        id = UUID.randomUUID().toString();
         description = weatherDto.getDescription();
         temperature = weatherDto.getTemperature();
         time = weatherDto.getTime();
         location = weatherDto.getLocation();
+    }
+
+    public void update( WeatherDto weatherDto )
+    {
+        description = weatherDto.getDescription();
+        temperature = weatherDto.getTemperature();
+        time = weatherDto.getTime();
+        location = weatherDto.getLocation();
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public double getTemperature()
+    {
+        return temperature;
+    }
+
+    public Date getTime()
+    {
+        return time;
+    }
+
+    public String getLocation()
+    {
+        return location;
     }
 }
